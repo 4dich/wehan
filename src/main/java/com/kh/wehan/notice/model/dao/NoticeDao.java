c75cea44fdf918d5431603c54b1cef62b8e6b0de
SERYE <SERYE@SERYE>
2020-04-06 19:27:25 +0900

    세례 커밋
    Signed-off-by: SERYE <SERYE@SERYE>

---
+++ src/main/java/com/kh/wehan/notice/model/dao/NoticeDao.java
@@ -0,0 +1,24 @@
+package com.kh.wehan.notice.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.wehan.notice.model.vo.Notice;
+
+@Repository("nDao")
+public class NoticeDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	/**
+	 * 공지사항 작성하기
+	 * @param n
+	 * @return
+	 */
+	public int insertNotice(Notice n) {
+		return sqlSession.insert("noticeMapper.insertNotice", n);
+	}
+
+}
---
+++ src/main/java/com/kh/wehan/notice/model/service/NoticeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kh.wehan.notice.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.wehan.notice.model.dao.NoticeDao;
+import com.kh.wehan.notice.model.vo.Notice;
+
+@Service("nService")
+public class NoticeServiceImpl implements NoticeService{
+	
+	@Autowired
+	private NoticeDao nDao;
+	
+	/**
+	 * 공지사항 작성하기
+	 */
+	@Override
+	public int insertNotice(Notice n) {
+		return nDao.insertNotice(n);
+	}
+
+}
