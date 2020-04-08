dfc7188d285874f56a1e0b3fda30b9ce97c5f4a4
SERYE <SERYE@SERYE>
2020-04-08 19:11:22 +0900

    세례 커밋

---
+++ src/main/java/com/kh/wehan/message/model/dao/MessageDao.java
@@ -0,0 +1,36 @@
+package com.kh.wehan.message.model.dao;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.wehan.member.model.vo.Member;
+import com.kh.wehan.message.model.vo.Message;
+
+@Repository("msgDao")
+public class MessageDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	/**
+	 * 메시지 리스트 가져오기
+	 * @return
+	 */
+	public ArrayList<Message> msgList() {
+		return (ArrayList)sqlSession.selectList("messageMapper.msgList");
+	}
+
+	/**
+	 * 메시지 친구 목록 가져오기
+	 * @param userId
+	 * @return
+	 */
+	public ArrayList<Member> msgFriendList(String userId) {
+		return (ArrayList)sqlSession.selectList("messageMapper.msgFriendList", userId);
+	}
+
+	
+}
---
+++ src/main/java/com/kh/wehan/message/model/service/MessageService.java
@@ -0,0 +1,23 @@
+package com.kh.wehan.message.model.service;
+
+import java.util.ArrayList;
+
+import com.kh.wehan.member.model.vo.Member;
+import com.kh.wehan.message.model.vo.Message;
+
+public interface MessageService {
+
+	/**
+	 * 메시지 리스트 가져오기
+	 * @return
+	 */
+	ArrayList<Message> msgList();
+
+	/**
+	 * 메시지 친구 목록 가져오기
+	 * @param userId
+	 * @return
+	 */
+	ArrayList<Member> msgFriendList(String userId);
+
+}
